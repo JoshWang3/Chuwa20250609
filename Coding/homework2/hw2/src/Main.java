a9674959813da835dab1a89bad13af16ac7a0cde
havit611 <113727005+havit611@users.noreply.github.com>
2025-06-15 01:54:12 -0400

    Update Main.java

--- Coding/homework2/hw2/src/Main.java
+++ Coding/homework2/hw2/src/Main.java
@@ -1,12 +1,15 @@
+// Main.java
 import java.time.LocalDate;
 import java.lang.reflect.Field;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         Department dept = new Department("Engineering");
+        Address addr1 = new Address("123 Main St", "A", "12345");
+        Address addr2 = new Address("222 Main Ave", "B", "67890");
 
-        Employee emp1 = new Employee("aEmpy", LocalDate.of(2000, 1, 1), dept, "87654321", "123 Main St");
-        Employee emp2 = new Employee("bEmpy", LocalDate.of(1990, 2, 2), dept,"12345678", "222 Main Ave");
+        Employee emp1 = new Employee("aEmpy", LocalDate.of(2000, 1, 1), dept, "87654321", addr1);
+        Employee emp2 = new Employee("bEmpy", LocalDate.of(1990, 2, 2), dept, "12345678", addr2);
 
         System.out.println("emp1 identityHashCode = " + System.identityHashCode(emp1));
         System.out.println("emp2 identityHashCode = " + System.identityHashCode(emp2));
