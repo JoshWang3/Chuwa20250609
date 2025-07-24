4b4a1c777cb3b63b5fc5e467a0f8a4c6acc52eb3
Yifei_Li <yifeili1230@gmail.com>
2025-07-23 21:18:56 -0700

    temp com

--- Coding/SingletonPatterns.java
+++ Coding/SingletonPatterns.java
@@ -1,13 +1,54 @@
-public class SingletonPattern {
-
-    //1.private static variable
-    private static SingletonPattern instance = new SingletonPattern();
-
-    //2. private constructor
-    private SingletonPattern(){
+public class SingletonPatterns {
+    public static void main(String[] args) {
+        EagerLoadSingleton els1 = EagerLoadSingleton.getInstance();
+        EagerLoadSingleton els2 = EagerLoadSingleton.getInstance();
+        els1.printMessage();
+        els2.printMessage();
+        els1.setMessage("new_message");
+        els2.printMessage();
     }
 
-    public static SingletonPattern getInstance(){
-        return instance;
+
+
+}
+
+class EagerLoadSingleton {
+    //1.private static instance - the only instance will be used
+    private static final EagerLoadSingleton INSTANCE = new EagerLoadSingleton();
+
+    private String message = "EagerLoadSingleton";
+
+    //2. private constructor
+    private EagerLoadSingleton(){
+    }
+
+
+    //3. get this pattern
+    public static EagerLoadSingleton getInstance(){
+        return INSTANCE;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void printMessage() {
+        System.out.println(this.message);
+    }
+}
+
+
+
+class LazyLoadSingleton {
+    private LazyLoadSingleton(){}
+
+    static {}
+
+    private static class SingletonHolder {
+        private static final LazyLoadSingleton INSTANCE = new LazyLoadSingleton();
+    }
+
+    public static LazyLoadSingleton getInstance(){
+        return SingletonHolder.INSTANCE;
     }
 }
